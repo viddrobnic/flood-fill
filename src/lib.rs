43bf6fd73aa8a509e7cadecc61e1044cc70dc55c
Vid Drobnič <me@viddrobnic.com>
2025-03-01 20:57:37 +0100

    setup cli parsing

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+use std::path::PathBuf;
+
+pub fn pre_process(input: PathBuf, output: PathBuf) {
+    println!("in: {:?}, out: {:?}", input, output);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use flood_fill::pre_process;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    PreProcess {
+        /// Path to input directory.
+        #[arg(long)]
+        input: PathBuf,
+
+        /// Path to output file
+        #[arg(long)]
+        output: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Command::PreProcess { input, output } => pre_process(input, output),
+    }
 }
